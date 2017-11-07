aa7c1f9b1af5e58a18c24cd2731048f70d8d0040
Mayank Kumar <kumarmayankdev2@gmail.com>
2017-11-07 12:22:01 +0530

    Removed unused files

--- src/reducers/Root.js
+++ src/reducers/Root.js
@@ -1,12 +1,10 @@
 import {combineReducers} from 'redux';
 
-import searchReducer from './Search';
 import profileReducer from './Profile';
 import reposReducer from './Repos';
 import entitiesReducer from './Entities';
 
 export default combineReducers({
-  search: searchReducer,
   profile: profileReducer,
   repos: reposReducer,
   entities: entitiesReducer,
