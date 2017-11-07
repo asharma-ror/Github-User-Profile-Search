e9778866a3e3fac7a7deb66edd2d5d6bd72c3ca5
Mayank Kumar <kumarmayankdev2@gmail.com>
2017-11-07 17:11:31 +0530

    Add missing timezone library import

--- src/components/weatherInfo.js
+++ src/components/weatherInfo.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {connect} from 'react-redux';
 
 import moment from 'moment';
+import momentTZ from 'moment-timezone';
 import _ from 'lodash';
 
 import {Card} from 'antd';
